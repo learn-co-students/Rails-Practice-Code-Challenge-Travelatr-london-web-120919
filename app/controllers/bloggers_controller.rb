31e28a71c9a6039a4191caa3a9157c84504b6fca
github username <github email address>
2020-01-16 09:16:20 +0000

    Done.

---
+++ app/controllers/bloggers_controller.rb
@@ -0,0 +1,18 @@
+class BloggersController < ApplicationController
+
+    def index
+        @bloggers = Blogger.all
+    end
+
+    def show
+        @blogger = Blogger.find(params[:id])
+    end
+
+    def new
+        @blogger = Blogger.new
+    end
+
+    def create
+        
+    end
+end
--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,12 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
 
+    def totalLikes
+       Post.all.where(blogger_id: self.id).collect{ |post| post[:likes]}.sum
+    end
+
+    def mostLikedPost
+        Post.all.where(blogger_id: self.id).collect{ |post| post[:likes]}}.sum
+    end
 end
